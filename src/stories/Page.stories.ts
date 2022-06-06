cadd92c93d0dbf0b14419a5274998fc25e766515
Yann Braga <yannbf@gmail.com>
2022-06-06 17:39:39 +0200

    setup

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -1,10 +1,11 @@
-import { moduleMetadata, Story, Meta } from '@storybook/angular';
-import { within, userEvent } from '@storybook/testing-library';
-import { CommonModule } from '@angular/common';
+import { moduleMetadata, Story, Meta } from '@storybook/angular'
+import { expect } from '@storybook/jest'
+import { within, userEvent } from '@storybook/testing-library'
+import { CommonModule } from '@angular/common'
 
-import Button from './button.component';
-import Header from './header.component';
-import Page from './page.component';
+import Button from './button.component'
+import Header from './header.component'
+import Page from './page.component'
 
 export default {
   title: 'Example/Page',
@@ -19,18 +20,20 @@ export default {
       imports: [CommonModule],
     }),
   ],
-} as Meta;
+} as Meta
 
 const Template: Story<Page> = (args: Page) => ({
   props: args,
-});
+})
 
-export const LoggedOut = Template.bind({});
+export const LoggedOut = Template.bind({})
 
 // More on interaction testing: https://storybook.js.org/docs/angular/writing-tests/interaction-testing
-export const LoggedIn = Template.bind({});
+export const LoggedIn = Template.bind({})
 LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
-};
+  const canvas = within(canvasElement)
+  const loginButton = await canvas.getByRole('button', { name: /Log in/i })
+  await userEvent.click(loginButton)
+  const logoutButton = await canvas.getByRole('button', { name: /Log out/i })
+  await expect(logoutButton).toBeInTheDocument()
+}
